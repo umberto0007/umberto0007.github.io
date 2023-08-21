a30eaf5bf21c8e7cde907fbc272d9cae7888a8fc
Anton Zheltyakov <79259503+umberto0007@users.noreply.github.com>
2023-08-21 19:20:59 +0300

    Delete projects/Deploy/vite-boilerplate directory

--- projects/Deploy/vite-boilerplate/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-
-export const pathResolver = (p: string) => resolve(__dirname, '.', p);
-
-export default defineConfig({
-    plugins: [react()],
-});
